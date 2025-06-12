15ca463091b4540f54d9cb860fe61ef14c7ea61e
Mimis <dim_kar@outlook.com>
2025-06-12 14:54:00 +0300

    (comments cleanup 2)

--- Watermarking-Generate/main.cpp
+++ Watermarking-Generate/main.cpp
@@ -10,7 +10,7 @@
 /*!
  *  \brief  This is a helper project for my Thesis with title:
  *			EFFICIENT IMPLEMENTATION OF WATERMARKING ALGORITHMS AND
- *			WATERMARK DETECTION IN IMAGE AND VIDEO USING GPU, CUDA version
+ *			WATERMARK DETECTION IN IMAGE AND VIDEO USING GPU.
  *          It generates a random matrix of floats and writes it to a file.
  *  \author Dimitris Karatzas
  */
@@ -21,20 +21,21 @@ int main(int argc, char* argv[])
         std::cerr << "Usage: " << argv[0] << " <rows> <cols> <seed> <output_file>\n";
         return EXIT_FAILURE;
     }
-    // Parse arguments
+	constexpr int maxSize = 65536;
+    //parse arguments
     const int rows = std::stoi(argv[1]);
     const int cols = std::stoi(argv[2]);
     const unsigned int seed = std::stoul(argv[3]);
     const std::string filename = argv[4];
-    if (rows <= 0 || cols <= 0 || rows >= 32768 || cols >= 32768)
+    if (rows <= 0 || cols <= 0 || rows > maxSize || cols > maxSize)
     {
-        std::cerr << "Rows and columns must be positive integers less than or equal to 32768.\n";
+        std::cerr << "Rows and columns must be positive integers less than or equal to " << maxSize <<".\n";
         return EXIT_FAILURE;
     }
     const int numElements = rows * cols;
     omp_set_num_threads(static_cast<int>(std::thread::hardware_concurrency()));
 
-    // Generate random numbers in parallel
+    //generate random numbers in parallel
     std::vector<float> randomNums(numElements);
 #pragma omp parallel
     {
@@ -44,17 +45,17 @@ int main(int argc, char* argv[])
         //watermark is a Gaussian distribution with mean 0 and standard deviation 1
         std::normal_distribution<float> distribution(0.0f, 1.0f);
 
-        // Compute range for each thread
+        //compute range for each thread
         const int threadElements = numElements / numThreads;
         const int start = threadId * threadElements;
         const int end = (threadId == numThreads - 1) ? numElements : start + threadElements;
 
-        // Generate random numbers for this thread
+        //generate random numbers for this thread
         for (int i = start; i < end; i++)
             randomNums[i] = distribution(generator);
     }
 
-    // Write the random numbers to the output file
+    //write the random numbers to the output file
     std::ofstream output(filename, std::ios::binary);
     if (!output)
     {
