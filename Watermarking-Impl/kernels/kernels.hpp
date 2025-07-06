0b449aa857d23db8de16dd25695ad6e3dcd896e0
Mimis <dim_kar@outlook.com>
2025-07-06 11:41:41 +0300

    minor OCL specific (ME kernel) speedup

--- Watermarking-Impl/kernels/kernels.hpp
+++ Watermarking-Impl/kernels/kernels.hpp
@@ -121,8 +121,6 @@ __kernel void me(__global const float* __restrict__ input,
     const int localId = get_local_id(0);
     const int widthLimit = paddedWidth == width ? 64 :get_group_id(0) == get_num_groups(0) - 1 ? 64 - (paddedWidth - width) : 64;
 
-    vstore_half8((float8)(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f), 0, &RxLocal[localId][0]);
-
     if (y >= height)
         return;
 
@@ -158,7 +156,7 @@ __kernel void me(__global const float* __restrict__ input,
     {
         float sum = 0.0f;
 #pragma unroll
-        for (int i = 0; i < 64; i++)
+        for (int i = 0; i < widthLimit; i++)
             sum += RxLocal[i][localId];
         rx[(outputIndex / 8) + localId] = sum;
     }
