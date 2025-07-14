259e762f1929d2714d8aba1d85bc9703f9c26a51
Mimis <dim_kar@outlook.com>
2025-07-14 09:29:39 +0300

    update watermarking generate sample project

--- Watermarking-Impl/samples/make_w.bat
+++ Watermarking-Impl/samples/make_w.bat
@@ -1,7 +1,8 @@
-CommonRandomMatrix.exe 480 720 28390211 w_480p.dat
-CommonRandomMatrix.exe 512 512 28390211 w_512.dat
-CommonRandomMatrix.exe 720 1280 28390211 w_720p.dat
-CommonRandomMatrix.exe 1078 1918 28390211 w_1078p.dat
-CommonRandomMatrix.exe 1080 1920 28390211 w_1080p.dat
-CommonRandomMatrix.exe 2160 3840 28390211 w_4k.dat
-CommonRandomMatrix.exe 2160 3872 28390211 w_4k_non_divisible.dat
+Watermarking-Generate.exe 480 720 28390211 w_480p.dat
+Watermarking-Generate.exe 512 512 28390211 w_512.dat
+Watermarking-Generate.exe 720 1280 28390211 w_720p.dat
+Watermarking-Generate.exe 1078 1918 28390211 w_1078p.dat
+Watermarking-Generate.exe 1080 1920 28390211 w_1080p.dat
+Watermarking-Generate.exe 2160 3840 28390211 w_4k.dat
+Watermarking-Generate.exe 2160 3872 28390211 w_4k_non_divisible.dat
+pause
