473902d133f56a6638131aebe27d8ff6d824642d
Mimis <dim_kar@outlook.com>
2025-07-15 09:53:19 +0300

    minor changes

--- Watermarking-Impl/eigen_utils.cpp
+++ Watermarking-Impl/eigen_utils.cpp
@@ -2,6 +2,7 @@
 #include "eigen_rgb_array.hpp"
 #include "eigen_utils.hpp"
 #include <Eigen/Dense>
+#include <optional>
 
 using namespace cimg_library;
 using namespace Eigen;
@@ -13,7 +14,7 @@ namespace eigen_utils
 		const auto rows = arrayRgb[0].rows();
 		const auto cols = arrayRgb[0].cols();
 		const int channels = alphaChannel.has_value() ? 4 : 3;
-		CImg<float> cimg_image(static_cast<unsigned int>(cols), static_cast<unsigned int>(rows), 1, channels);
+		CImg<float> output(static_cast<unsigned int>(cols), static_cast<unsigned int>(rows), 1, channels);
 	#pragma omp parallel for
 		for (int y = 0; y < rows; ++y)
 		{
@@ -21,13 +22,13 @@ namespace eigen_utils
 			{
 				for (int channel = 0; channel < 3; channel++)
 				{
-					cimg_image(x, y, 0, channel) = arrayRgb[channel](y, x);
+					output(x, y, 0, channel) = arrayRgb[channel](y, x);
 				}
 				if (channels == 4)
-					cimg_image(x, y, 0, 3) = (*alphaChannel)(x, y);
+					output(x, y, 0, 3) = (*alphaChannel)(x, y);
 			}
 		}
-		return cimg_image;
+		return output;
 	}
 
 	void cimgAlphaZero(cimg_library::CImg<float>& rgbImage, const CImg<float>& alphaChannel)
@@ -50,13 +51,13 @@ namespace eigen_utils
 	{
 		const int rows = rgbImage.height();
 		const int cols = rgbImage.width();
-		EigenArrayRGB rgb_array = { ArrayXXf(rows,cols), ArrayXXf(rows,cols), ArrayXXf(rows, cols) };
+		EigenArrayRGB output = { ArrayXXf(rows,cols), ArrayXXf(rows,cols), ArrayXXf(rows, cols) };
 	#pragma omp parallel for
 		for (int x = 0; x < rgbImage.width(); x++)
 			for (int y = 0; y < rgbImage.height(); y++)
 				for (int channel = 0; channel < 3; channel++)
-					rgb_array[channel](y, x) = rgbImage(x, y, 0, channel);
-		return rgb_array;
+					output[channel](y, x) = rgbImage(x, y, 0, channel);
+		return output;
 	}
 
 	ArrayXXf eigenRgbToGray(const EigenArrayRGB& arrayRgb, const float rWeight, const float gWeight, const float bWeight)
