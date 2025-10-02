41f8ab18879cc81ac17fd3dc30dcedb1ab141719
Mimis <dim_kar@outlook.com>
2025-10-02 21:13:39 +0300

    minor refactor in test helper

--- Watermarking-Impl-tests/FileDeleter.h
+++ Watermarking-Impl-tests/FileDeleter.h
@@ -11,11 +11,13 @@ class FileDeleter {
     std::filesystem::path filePath;
 public:
     explicit FileDeleter(const std::filesystem::path& path) : filePath(path) {}
-    ~FileDeleter() {
+    ~FileDeleter() 
+    {
+        using std::filesystem::exists;
+        using std::filesystem::remove;
         std::error_code ec;
-        if (std::filesystem::exists(filePath, ec))
-            if (!std::filesystem::remove(filePath, ec))
-                std::cerr << "Warning: Failed to delete file " << filePath << ": " << ec.message() << "\n"; 
+        if (exists(filePath, ec) && !remove(filePath, ec))
+            std::cerr << "Warning: Failed to delete file " << filePath << ": " << ec.message() << "\n"; 
     }
     FileDeleter(const FileDeleter&) = delete;
     FileDeleter& operator=(const FileDeleter&) = delete;
