df8d2ea6baf2532d2e30fbac82b986a00d92cb72
Mimis <dim_kar@outlook.com>
2025-10-26 19:51:24 +0200

    Refactor class/file names to be more readable part2

--- Watermarking-Impl/eigen_utils.hpp
+++ Watermarking-Impl/eigen_utils.hpp
@@ -17,7 +17,7 @@ enum IMAGE_TYPE
 namespace eigen_utils
 {
 	cimg_library::CImg<float> eigenRgbToCimg(const EigenArrayRGB& imageRgb, const std::optional<AlphaBuffer>& alphaChannel);
-	void cimgAlphaZero(cimg_library::CImg<float>& rgbImage, const cimg_library::CImg<float>& alphaChannel);
+	void cimgAlphaZero(cimg_library::CImg<float>& rgbImage, const AlphaBuffer& alphaChannel);
 	EigenArrayRGB cimgToEigenRgb(const cimg_library::CImg<float>& rgbImage);
 	void setThreadsToPhysicalCores();
 	inline EigenArrayRGB makeEigenRGB(int rows, int cols)
