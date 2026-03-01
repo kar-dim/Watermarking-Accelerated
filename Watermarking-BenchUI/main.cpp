2a3bf14809676e00135420fd70f2ce1f96b7ffff
Mimis <dim_kar@outlook.com>
2026-03-01 19:25:08 +0200

    Bench: remove useless files + fix custom font

--- Watermarking-BenchUI/main.cpp
+++ Watermarking-BenchUI/main.cpp
@@ -1,19 +1,23 @@
 #include "WatermarkingBenchUI.h"
-#include <QtWidgets/QApplication>
 #include <QFile>
+#include <QFont>
 #include <QFontDatabase>
 #include <QIcon>
+#include <QString>
+#include <QtWidgets/QApplication>
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon(":/assets/watermark_icon.ico"));
-
+    // apply the stylesheet
     QFile styleFile(":/stylesheets/main.qss");
     styleFile.open(QFile::ReadOnly);
-    // apply the stylesheet
     app.setStyleSheet(QString(styleFile.readAll()));
-    QFontDatabase::addApplicationFont(":/assets/fonts/TitilliumWeb-Regular.ttf");
-
+    // apply the font
+    const int fontId = QFontDatabase::addApplicationFont(":/assets/fonts/TitilliumWeb-Regular.ttf");
+    if (fontId != -1)
+        app.setFont(QFont(QFontDatabase::applicationFontFamilies(fontId).at(0), 10));
+    // show the main window
     WatermarkingBenchUI window;
     window.show();
     return app.exec();
