673400435079bd37f0aeed4c9cca014d5c4978d3
Mimis <dim_kar@outlook.com>
2026-05-05 19:05:17 +0300

    buffer.hpp: reduce duplication

--- Watermarking-Impl/buffer.hpp
+++ Watermarking-Impl/buffer.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "cimg_init.h"
 #include <cstdint>
+using Gray8BufferIO = cimg_library::CImg<uint8_t>;
+using FloatBufferIO = cimg_library::CImg<float>;
 #if defined(_USE_CUDA_)
 #include "CudaArray.hpp"
 #include <cuda_runtime.h>
@@ -10,8 +12,6 @@ using ImageOutputBuffer = CudaArray<uint8_t>;
 using Gray8Buffer = CudaArray<uint8_t>;
 using Gray16Buffer = CudaArray<uint16_t>;
 using FlagBuffer = CudaArray<int32_t>;
-using Gray8BufferIO = cimg_library::CImg<uint8_t>;
-using FloatBufferIO = cimg_library::CImg<float>;
 #elif defined(_USE_OPENCL_)
 #include "OclArray.hpp"
 #include "opencl_init.h"
@@ -21,8 +21,6 @@ using ImageOutputBuffer = OclArray<uint8_t>;
 using Gray8Buffer = OclArray<uint8_t>;
 using Gray16Buffer = OclArray<uint16_t>;
 using FlagBuffer = OclArray<int32_t>;
-using Gray8BufferIO = cimg_library::CImg<uint8_t>;
-using FloatBufferIO = cimg_library::CImg<float>;
 #elif defined(_USE_EIGEN_)
 #include "ImageEigenBuffer.hpp"
 #include "ImageEigenOutputBuffer.hpp"
@@ -30,7 +28,5 @@ using FloatBufferIO = cimg_library::CImg<float>;
 using ImageBuffer = ImageEigenBuffer;
 using ImageOutputBuffer = ImageEigenOutputBuffer;
 using Gray8Buffer = Eigen::Array<uint8_t, Eigen::Dynamic, Eigen::Dynamic>;
-using Gray8BufferIO = cimg_library::CImg<uint8_t>;
 using Gray16Buffer = Eigen::Array<uint16_t, Eigen::Dynamic, Eigen::Dynamic>;
-using FloatBufferIO = cimg_library::CImg<float>;
 #endif
